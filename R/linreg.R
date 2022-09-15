ef971eb620774a919f774227261b8c4de13b8051
Your Name <you@example.com>
2022-09-15 22:40:45 +0800

    fix the problem with NA in sqrt

--- R/linreg.R
+++ R/linreg.R
@@ -12,7 +12,7 @@
     the_degrees_of_freedom <- n-p
     the_residual_variance <- t(the_residuals) %*% the_residuals / the_degrees_of_freedom
     the_variance_of_the_regression_coefficients <- drop(the_residual_variance) * solve((t(x)%*%x))
-    t_value <- drop(regressions_coefficients) / sqrt(diag(the_variance_of_the_regression_coefficients))
+    t_value <- regressions_coefficients / sqrt(diag(the_variance_of_the_regression_coefficients))
     p_value <- pt(regressions_coefficients,the_degrees_of_freedom)
     
     return(t_value)
