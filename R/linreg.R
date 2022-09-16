555ad327df8c424cda368791e0689749e9950bca
kiriakospapa <kyriakospapadopoulosauto@gmail.com>
2022-09-16 14:13:46 +0300

    Added function `print` but it doeasn't work properly

--- R/linreg.R
+++ R/linreg.R
@@ -1,5 +1,13 @@
 # Defining the class LinReg  
-LinReg <- setRefClass("LinReg", fields = list(t_values = "matrix", p_values = "matrix"))
+LinReg <- setRefClass("LinReg", fields = list(t_values = "matrix", p_values = "matrix", regressions_coefficients = "matrix"),
+                      methods = list(
+                      show = function(){
+                        names <- rownames(regressions_coefficients)
+                        name <- colnames(regressions_coefficients)
+                        output <- list(regressions_coefficients) 
+                        print("hey eric")
+                      }  
+))
 
 
 linreg<-function(formula,data){
@@ -31,7 +39,7 @@ linreg<-function(formula,data){
     # Finding the t values for each coefficient
     t_values <- regressions_coefficients / sqrt(diag(the_variance_of_the_regression_coefficients))
     p_values <- pt(regressions_coefficients,the_degrees_of_freedom)
-    linreg_object <- LinReg(t_values = t_values, p_values = p_values)
+    linreg_object <- LinReg(t_values = t_values, p_values = p_values, regressions_coefficients = regressions_coefficients)
     
     return(linreg_object)
 } 
